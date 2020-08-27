473cce96e8e27561f163d0a2e639cb33ef8ef445
unknown <nesrineessaies@gmail.com>
2020-08-27 10:19:53 +0200

    template of the chat app

---
+++ resources/js/components/message.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+         <li class="list-group-item" :class="className"><slot></slot> </li>
+        <small class="badge float-right " :class="classbadge">You</small>
+
+</div>
+</template>
+
+<script>
+    export default {
+        props:[
+            'color',
+        ],
+
+        computed: {
+            className(){
+                console.log('list-group-item-'+this.color);
+                return 'list-group-item-'+this.color;
+            },
+            classbadge(){
+                 return 'badge-'+this.color;
+
+            }
+        },
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
